a0c37eea48c90c43c38c2165a4c6aefc9f1aa9ba
dongchanglong <1794898573@qq.com>
2022-10-12 15:53:53 +0800

    Initial commit

---
+++ CloudAlibaba-Nacos-9002/src/main/java/com/dongchanglong/cloudalibabanacos9002/DemoController.java
@@ -0,0 +1,16 @@
+package com.dongchanglong.cloudalibabanacos9002;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @Value("${server.port}")
+    private String serverPort;
+    @GetMapping(value = "/mashibing")
+    public String getServerPort(){
+
+        return "Hello Nacos Discovery"+serverPort;
+    }
+}
---
+++ src/main/java/com/dongchanglong/springcloudalibaba/SpringCloudAlibabaApplication.java
@@ -0,0 +1,14 @@
+package com.dongchanglong.springcloudalibaba;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaApplication.class, args);
+    }
+
+}
