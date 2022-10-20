2d328ea2abc837c0517407d65e48bf64462da58a
dongchanglong <1794898573@qq.com>
2022-10-20 21:36:44 +0800

    Sentinel

---
+++ CloudAlibaba-Sentinel-8401/src/main/java/com/dongchanglong/cloudalibabasentinel8401/CloudAlibabaSentinel8401Application.java
@@ -0,0 +1,13 @@
+package com.dongchanglong.cloudalibabasentinel8401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAlibabaSentinel8401Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAlibabaSentinel8401Application.class, args);
+    }
+
+}
---
+++ CloudAlibaba-Sentinel-8401/src/test/java/com/dongchanglong/cloudalibabasentinel8401/CloudAlibabaSentinel8401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongchanglong.cloudalibabasentinel8401;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAlibabaSentinel8401ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
